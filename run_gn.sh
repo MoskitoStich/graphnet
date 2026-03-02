add70a2924cebb00fa95ffe978b130bea304ac5c
Elias Fischer <moskito.stich2.0@gmail.com>
2026-03-02 16:12:11 -0600

    vor Urlaub Philippinen Vietnam

--- run_gn.sh
+++ run_gn.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-# 1. Alte Pfade löschen
+# 1. delete old env vars to avoid conflicts
 unset PYTHONPATH
 unset PATH
 
-# 2. IceCube Basis laden
+# 2. load IceCube env
 eval `/cvmfs/icecube.opensciencegrid.org/py3-v4.3.0/setup.sh`
 
-# 3. Deine Umgebung DAVOR schalten (Damit er dein PyTorch nimmt, nicht das alte)
-# ACHTUNG: Pfad muss genau stimmen!
+# 3. using new pytorch version from venv
 VENV_SITE="/data/user/efischer/graphnet/graphnet_venv/lib/python3.11/site-packages"
 export PYTHONPATH=$VENV_SITE:$PYTHONPATH
 
-# Venv aktivieren
+# activate Venv 
 source /data/user/efischer/graphnet/graphnet_venv/bin/activate
 
-# 4. Info ausgeben
+# 4. info output
 echo "Starte mit PYTHONPATH: $PYTHONPATH"
 
-# 5. Den Befehl IN DER ICETRAY-UMGEBUNG ausführen
+# 5. execute the python script with the IceCube env
 exec /cvmfs/icecube.opensciencegrid.org/py3-v4.3.0/RHEL_7_x86_64/metaprojects/icetray/v1.9.2/env-shell.sh python "$@"
